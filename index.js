727d5ba55cd4646613c13e1c624cc408cffaffce
Sean Hayes <hayessea000@outlook.com>
2023-04-26 16:52:33 -0500

    adding more to shape classes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const makeShape = require('./lib/shapes');
 
 inquirer
   .prompt([
     {
-      type: 'input',
-      name: 'text',
-      message: 'What 3 letter do you want?',
+        type: 'input',
+        name: 'text',
+        message: 'What 3 charactors do you want?',
     },
     {
         type: 'input',
@@ -14,10 +15,10 @@ inquirer
         message: 'What color do you want the text?',
     },
     {
-      type: 'list',
-      message: 'What shape do you want?',
-      name: 'shape',
-      choices: ['Triangle', 'Circle', 'Square'],
+        type: 'list',
+        message: 'What shape do you want?',
+        name: 'shape',
+        choices: ['Triangle', 'Circle', 'Square'],
     },
     {
         type: 'input',
@@ -28,14 +29,24 @@ inquirer
   .then((data) => {
     const filename = 'logo.svg';
 
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+    fs.writeFile(filename, writeLogo(data), (err) =>
       err ? console.log(err) : console.log('Generated logo.svg')
     );
   });
-{/* <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+let writeLogo = function(){
+    return`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    
+    ${createdShape}
+    
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${data.text}</text>
+    
+    </svg>`
+}
+/* <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
 <circle cx="150" cy="100" r="80" fill="green" />
 
 <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
 
-</svg> */}
+</svg> */
