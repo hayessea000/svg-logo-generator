06076f6f9062b227d2a54e9906f32469596f7634
Sean Hayes <hayessea000@outlook.com>
2023-04-27 16:39:54 -0500

    cleaned up the code

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const makeShape = require('./lib/shapes');
-// const path = require('path')
-// const OUTPUT_DIR =path.resolve(__dirname, 'examples')
-// const outputPath =path.join(OUTPUT_DIR, 'logo.svg')
 
 inquirer
   .prompt([
@@ -11,6 +8,13 @@ inquirer
         type: 'input',
         name: 'text',
         message: 'What 3 charactors do you want?',
+        validate: (text) => {
+          if (text.length > 3) {
+             return `Incorrect asnwer, must be 3 or less charactors`;
+          }else{
+            return true;
+          }
+       }
     },
     {
         type: 'input',
@@ -60,17 +64,4 @@ inquirer
     fs.writeFile(filename, writeLogo(data), (err) =>
       err ? console.log(err) : console.log('Generated logo.svg')
     );
-    // function exampleFiles(){
-    //     if(!fs.existsSync(OUTPUT_DIR)){
-    //         fs.mkdirSync(OUTPUT_DIR)
-    //     }fs.writeFileSync(outputPath, render(filename))
-    // }exampleFiles()
   });
-
-/* <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
-<circle cx="150" cy="100" r="80" fill="green" />
-
-<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
-
-</svg> */
